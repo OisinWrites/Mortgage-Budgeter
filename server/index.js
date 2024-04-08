e6ae6fb4ede9b85a3da800bc41266d8742ad9d79
OisinWrites <oisin.banville@gmail.com>
2024-04-08 15:17:18 +0100

    Edit html file and change path syntax in server

--- server/index.js
+++ server/index.js
@@ -3,14 +3,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, '..', 'client/build')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from server!' });
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
